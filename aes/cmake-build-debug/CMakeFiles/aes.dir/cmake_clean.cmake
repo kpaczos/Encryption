e089c62b98b963abaa0a5b073d62aa5defccedab
Kamil Paczos <61743455+kpaczos@users.noreply.github.com>
2021-08-23 11:48:59 +0200

    Add files via upload

---
+++ aes/cmake-build-debug/CMakeFiles/aes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aes.dir/main.cpp.obj"
+  "aes.exe"
+  "aes.exe.manifest"
+  "aes.pdb"
+  "libaes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
